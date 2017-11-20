ae271f0e28cfad3049c89ae47e228028864c312b
r_liu <ruoshi98@github.com>
2017-11-20 10:40:07 -0500

    printJapanese

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,7 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello World!!!!!!!");
   }
+  public static printJapanese(){
+    System.out.ptintln("山")；
+  }
 }
